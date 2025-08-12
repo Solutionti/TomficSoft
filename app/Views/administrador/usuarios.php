6bf851f7700816a504a8dc23730720f635af31ed
Desarrollador Jerson Galvez <desarrollo.gocho@gmail.com>
2025-08-12 18:09:31 -0500

    login quedo listo

--- app/Views/administrador/usuarios.php
+++ app/Views/administrador/usuarios.php
@@ -19,8 +19,8 @@
           <div class="content-wrapper">
             <div class="page-header flex-wrap">
               <div class="header-left">
-                <button class="btn btn-primary mb-2 mb-md-0 me-2">Terminar el conteo</button>
-                <button class="btn btn-outline-primary bg-white mb-2 mb-md-0">Exportar base de datos</button>
+                <button class="btn btn-primary mb-2 mb-md-0 me-2">Agregar Usuarios</button>
+                <!-- <button class="btn btn-outline-primary bg-white mb-2 mb-md-0">Exportar base de datos</button> -->
               </div>
               <div class="header-right d-flex flex-wrap mt-2 mt-sm-0">
                 <div class="d-flex align-items-center">
@@ -39,11 +39,11 @@
                     <table class="table table-striped">
                         <thead >
                           <tr >
-                            <th class="color-morado"> User </th>
-                            <th class="color-morado"> First name </th>
-                            <th class="color-morado"> Progress </th>
-                            <th class="color-morado"> Amount </th>
-                            <th class="color-morado"> Deadline </th>
+                            <th class="color-morado text-white"> User </th>
+                            <th class="color-morado text-white"> First name </th>
+                            <th class="color-morado text-white"> Progress </th>
+                            <th class="color-morado text-white"> Amount </th>
+                            <th class="color-morado text-white"> Deadline </th>
                           </tr>
                         </thead>
                         <tbody>
@@ -72,71 +72,6 @@
                             </td>
                             <td> $245.30 </td>
                             <td> July 1, 2015 </td>
-                          </tr>
-                          <tr>
-                            <td class="py-1">
-                              <img src="../../assets/images/faces-clipart/pic-3.png" alt="image" />
-                            </td>
-                            <td> John Richards </td>
-                            <td>
-                              <div class="progress">
-                                <div class="progress-bar bg-warning" role="progressbar" style="width: 90%" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
-                              </div>
-                            </td>
-                            <td> $138.00 </td>
-                            <td> Apr 12, 2015 </td>
-                          </tr>
-                          <tr>
-                            <td class="py-1">
-                              <img src="../../assets/images/faces-clipart/pic-4.png" alt="image" />
-                            </td>
-                            <td> Peter Meggik </td>
-                            <td>
-                              <div class="progress">
-                                <div class="progress-bar bg-primary" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-                              </div>
-                            </td>
-                            <td> $ 77.99 </td>
-                            <td> May 15, 2015 </td>
-                          </tr>
-                          <tr>
-                            <td class="py-1">
-                              <img src="../../assets/images/faces-clipart/pic-1.png" alt="image" />
-                            </td>
-                            <td> Edward </td>
-                            <td>
-                              <div class="progress">
-                                <div class="progress-bar bg-danger" role="progressbar" style="width: 35%" aria-valuenow="35" aria-valuemin="0" aria-valuemax="100"></div>
-                              </div>
-                            </td>
-                            <td> $ 160.25 </td>
-                            <td> May 03, 2015 </td>
-                          </tr>
-                          <tr>
-                            <td class="py-1">
-                              <img src="../../assets/images/faces-clipart/pic-2.png" alt="image" />
-                            </td>
-                            <td> John Doe </td>
-                            <td>
-                              <div class="progress">
-                                <div class="progress-bar bg-info" role="progressbar" style="width: 65%" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100"></div>
-                              </div>
-                            </td>
-                            <td> $ 123.21 </td>
-                            <td> April 05, 2015 </td>
-                          </tr>
-                          <tr>
-                            <td class="py-1">
-                              <img src="../../assets/images/faces-clipart/pic-3.png" alt="image" />
-                            </td>
-                            <td> Henry Tom </td>
-                            <td>
-                              <div class="progress">
-                                <div class="progress-bar bg-warning" role="progressbar" style="width: 20%" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100"></div>
-                              </div>
-                            </td>
-                            <td> $ 150.00 </td>
-                            <td> June 16, 2015 </td>
                           </tr>
                         </tbody>
                       </table>  
